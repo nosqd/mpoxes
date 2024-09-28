f6a43d7d6dc4ab434f9e4723abf470541c1cb70b
nosqd <enderofficial.ml@gmail.com>
2024-09-28 22:39:07 +0300

    connect window + some renamings

--- Source/Game/MPoxes.cpp
+++ Source/Game/MPoxes.cpp
@@ -23,17 +23,20 @@ constexpr int DESIGN_HEIGHT = 562;
 
 class Game {
 public:
-    bool isServer = false;
+    bool is_server = false;
     Camera2D camera{};
-    Player *player1 = nullptr;
+    Player *local_player = nullptr;
     bool running = false;
-    int idCounter = 0;
-    std::unordered_map<int, Player *> playersMap;
-    std::unordered_map<int, Vector2> playersWishDirMap;
+    int id_counter = 0;
+    std::unordered_map<int, Player *> players;
+    std::unordered_map<int, Vector2> players_wish_dirs;
 
     ENetHost *server{};
     ENetHost *client{};
     ENetPeer *client_peer{};
+    std::string connect_modal_error_message;
+    char connect_modal_host[256] = "localhost";
+    char connect_modal_port[6] = "7777";
 
     void Setup();
 
@@ -44,12 +47,14 @@ public:
     void RenderImGui();
 
     void Shutdown();
+
+    void ClientConnect(std::string host, int port);
 };
 
 void Game::Setup() {
     running = true;
     TraceLog(LOG_INFO, "starting mpoxes.");
-    if (!isServer) {
+    if (!is_server) {
         InitWindow(DESIGN_WIDTH, DESIGN_HEIGHT, "mpoxes");
 
         Camera2D camera = {0};
@@ -58,7 +63,7 @@ void Game::Setup() {
         rlImGuiSetup(true);
     }
 
-    if (isServer) {
+    if (is_server) {
         ENetAddress address;
 
         address.host = ENET_HOST_ANY;
@@ -71,33 +76,17 @@ void Game::Setup() {
             exit(EXIT_FAILURE);
         }
     } else {
-        ENetEvent event;
-        ENetAddress address;
-
         client = enet_host_create(nullptr, 1, 2, 0, 0);
 
         if (client == nullptr) {
             TraceLog(LOG_FATAL, "An error occurred while trying to create an ENet client host.");
             exit(EXIT_FAILURE);
         }
-
-        enet_address_set_host(&address, "localhost");
-        address.port = 7777;
-
-        client_peer = enet_host_connect(client, &address, 2, 0);
-
-        if (enet_host_service(client, &event, 5000) > 0 && event.type == ENET_EVENT_TYPE_CONNECT) {
-            TraceLog(LOG_INFO, "Connection to server succeeded.");
-        } else {
-            enet_peer_reset(client_peer);
-            TraceLog(LOG_FATAL, "Connection to server failed.");
-            exit(EXIT_FAILURE);
-        }
     }
 }
 
 void Game::Update(float dt) {
-    if (!isServer) {
+    if (!is_server) {
         float scale = fmin(static_cast<float>(GetScreenWidth()) / DESIGN_WIDTH,
                            static_cast<float>(GetScreenHeight()) / DESIGN_HEIGHT);
 
@@ -106,22 +95,22 @@ void Game::Update(float dt) {
         camera.zoom = scale;
     }
 
-    for (auto p: playersMap) {
-        p.second->Update(dt, playersWishDirMap[p.first]);
+    for (auto p: players) {
+        p.second->Update(dt, players_wish_dirs[p.first]);
     }
 
-    if (isServer) {
+    if (is_server) {
         ENetEvent event;
         while (enet_host_service(server, &event, 0) > 0) {
             switch (event.type) {
                 case ENET_EVENT_TYPE_CONNECT: {
                     TraceLog(LOG_INFO, "New client connected");
-                    auto* p = new Player(++idCounter, Vector2(0, 0));
-                    playersMap[p->id] = p;
-                    playersWishDirMap[p->id] = Vector2(0, 0);
+                    auto *p = new Player(++id_counter, Vector2(0, 0));
+                    players[p->id] = p;
+                    players_wish_dirs[p->id] = Vector2(0, 0);
                     event.peer->data = reinterpret_cast<void *>(p->id);
 
-                    for (auto& players_pair : playersMap) {
+                    for (auto &players_pair: players) {
                         auto join_packet = server_join_packet(players_pair.second->id, players_pair.second->position);
                         enet_host_broadcast(server, 0, join_packet);
                     }
@@ -132,11 +121,11 @@ void Game::Update(float dt) {
                 }
                 case ENET_EVENT_TYPE_RECEIVE: {
                     auto pid = reinterpret_cast<int>(event.peer->data);
-                    auto player = playersMap[pid];
-                    auto data = reinterpret_cast<char*>(event.packet->data);
+                    auto player = players[pid];
+                    auto data = reinterpret_cast<char *>(event.packet->data);
                     if (data[0] == 'm') {
                         auto wishDir = client_move_packet_get_input(data);
-                        playersWishDirMap[pid] = moveDirectionFrom(wishDir);
+                        players_wish_dirs[pid] = moveDirectionFrom(wishDir);
 
                         auto bc_packet2 = server_move_packet(pid, wishDir);
                         enet_host_broadcast(server, 0, bc_packet2);
@@ -154,43 +143,45 @@ void Game::Update(float dt) {
             }
         }
     } else {
-        auto packet = client_move_packet(moveDirectionTo(Input::GetMoveDir()));
-        enet_peer_send(client_peer, 0, packet);
+        if (client_peer != nullptr) {
+            auto packet = client_move_packet(moveDirectionTo(Input::GetMoveDir()));
+            enet_peer_send(client_peer, 0, packet);
 
-        ENetEvent event;
-        while (enet_host_service(client, &event, 0) > 0) {
-            switch (event.type) {
-                case ENET_EVENT_TYPE_RECEIVE: {
-                    auto data = reinterpret_cast<char*>(event.packet->data);
-                    if (data[0] == 'j') {
-                        int id = server_join_packet_get_id(data);
-                        Vector2 pos = server_join_packet_get_position(data);
-                        Player* p = new Player(id, pos);
-                        playersMap[id] = p;
-                        playersWishDirMap[id] = Vector2(0,0);
+            ENetEvent event;
+            while (enet_host_service(client, &event, 0) > 0) {
+                switch (event.type) {
+                    case ENET_EVENT_TYPE_RECEIVE: {
+                        auto data = reinterpret_cast<char *>(event.packet->data);
+                        if (data[0] == 'j') {
+                            int id = server_join_packet_get_id(data);
+                            Vector2 pos = server_join_packet_get_position(data);
+                            Player *p = new Player(id, pos);
+                            players[id] = p;
+                            players_wish_dirs[id] = Vector2(0, 0);
 
-                        TraceLog(LOG_INFO, TextFormat("New player joined: %d", id));
-                    } else if (data[0] == 'm') {
-                        int id = server_move_packet_get_id(data);
-                        Vector2 wishDir = moveDirectionFrom(server_move_packet_get_input(data));
+                            TraceLog(LOG_INFO, TextFormat("New player joined: %d", id));
+                        } else if (data[0] == 'm') {
+                            int id = server_move_packet_get_id(data);
+                            Vector2 wishDir = moveDirectionFrom(server_move_packet_get_input(data));
 
-                        playersWishDirMap[id] = wishDir;
-                    } else if (data[0] == 'h') {
-                        auto pid = server_hello_packet_get_id(data);
-                        player1 = playersMap[pid];
+                            players_wish_dirs[id] = wishDir;
+                        } else if (data[0] == 'h') {
+                            auto pid = server_hello_packet_get_id(data);
+                            local_player = players[pid];
+                        }
+
+                        enet_packet_destroy(event.packet);
+                        break;
                     }
-
-                    enet_packet_destroy(event.packet);
-                    break;
+                    case ENET_EVENT_TYPE_DISCONNECT: {
+                        TraceLog(LOG_FATAL, "Client disconnected");
+                        exit(EXIT_FAILURE);
+                        return;
+                    }
+                    case ENET_EVENT_TYPE_NONE:
+                    case ENET_EVENT_TYPE_CONNECT:
+                        break;
                 }
-                case ENET_EVENT_TYPE_DISCONNECT: {
-                    TraceLog(LOG_FATAL, "Client disconnected");
-                    exit(EXIT_FAILURE);
-                    return;
-                }
-                case ENET_EVENT_TYPE_NONE:
-                case ENET_EVENT_TYPE_CONNECT:
-                    break;
             }
         }
     }
@@ -201,7 +192,7 @@ void Game::Render() {
     ClearBackground(BLACK);
 
     BeginMode2D(camera);
-    for (auto p: playersMap) {
+    for (auto p: players) {
         p.second->Render();
     }
 
@@ -211,25 +202,50 @@ void Game::Render() {
 void Game::RenderImGui() {
     rlImGuiBegin();
 
-    auto drawList = ImGui::GetForegroundDrawList();
+    ImGui::SetNextWindowPos(ImVec2(16.f, 8.f));
+    ImGui::Begin("Debug", nullptr,
+                 ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar |
+                 ImGuiWindowFlags_AlwaysAutoResize);
 
-    int line = 0;
-    drawList->AddText(ImVec2(16.f, ++line * 14.f), 0xFFFFFFFF, TextFormat("FPS: %d", GetFPS()));
-    drawList->AddText(ImVec2(16.f, ++line * 14.f), 0xFFFFFFFF, TextFormat("----------"));
-    for (auto p: playersMap) {
-        drawList->AddText(ImVec2(16.f, ++line * 14.f), 0xFFFFFFFF, TextFormat("Player %d", p.first));
-        drawList->AddText(ImVec2(16.f, ++line * 14.f), 0xFFFFFFFF,
-                          TextFormat("Position: %f %f", p.second->position.x, p.second->position.y));
-        drawList->AddText(ImVec2(16.f, ++line * 14.f), 0xFFFFFFFF,
-                          TextFormat("Wishdir: %f %f", playersWishDirMap[p.first].x, playersWishDirMap[p.first].y));
-        drawList->AddText(ImVec2(16.f, ++line * 14.f), 0xFFFFFFFF, TextFormat("----------"));
+    ImGui::Text("mpoxes");
+    ImGui::Text(TextFormat("FPS: %d", GetFPS()));
+    ImGui::Separator();
+    for (auto p: players) {
+        ImGui::Text(TextFormat("Player %d", p.first));
+        ImGui::Text(TextFormat("Position: %f %f", p.second->position.x, p.second->position.y));
+        ImGui::Text(TextFormat("Wishdir: %f %f", players_wish_dirs[p.first].x, players_wish_dirs[p.first].y));
+        ImGui::Separator();
     }
+
+    ImGui::End();
+
+    if (client_peer == nullptr) {
+        ImGui::SetNextWindowSize(ImVec2(300, 200), ImGuiCond_FirstUseEver);
+        ImGui::SetNextWindowPos(ImVec2(ImGui::GetIO().DisplaySize.x * 0.5f, ImGui::GetIO().DisplaySize.y * 0.5f),
+                                ImGuiCond_FirstUseEver, ImVec2(0.5f, 0.5f));
+
+        if (ImGui::Begin("Connect to Server", nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+            ImGui::InputText("Host", connect_modal_host, IM_ARRAYSIZE(connect_modal_host));
+            ImGui::InputText("Port", connect_modal_port, IM_ARRAYSIZE(connect_modal_port));
+
+            if (ImGui::Button("Connect")) {
+                ClientConnect(connect_modal_host, std::atoi(connect_modal_port));
+            }
+
+            if (!connect_modal_error_message.empty()) {
+                ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), connect_modal_error_message.c_str());
+            }
+
+            ImGui::End();
+        }
+    }
+
 
     rlImGuiEnd();
 }
 
 void Game::Shutdown() {
-    if (!isServer) {
+    if (!is_server) {
         rlImGuiShutdown();
         CloseWindow();
 
@@ -254,8 +270,27 @@ void Game::Shutdown() {
         enet_host_destroy(client);
     }
 
-    if (isServer) {
+    if (is_server) {
         enet_host_destroy(server);
+    }
+}
+
+void Game::ClientConnect(std::string host, int port) {
+    ENetEvent event;
+    ENetAddress address;
+
+    enet_address_set_host(&address, host.c_str());
+    address.port = port;
+
+    client_peer = enet_host_connect(client, &address, 2, 0);
+
+    if (enet_host_service(client, &event, 1000) > 0 && event.type == ENET_EVENT_TYPE_CONNECT) {
+        TraceLog(LOG_INFO, "Connection to server succeeded.");
+    } else {
+        enet_peer_reset(client_peer);
+        client_peer = nullptr;
+        connect_modal_error_message = "Could not connect to server.";
+        TraceLog(LOG_ERROR, "Connection to server failed.");
     }
 }
 
@@ -263,9 +298,9 @@ int main(int argc, char *argv[]) {
     Game game;
 
     if (argc > 1 && std::strcmp(argv[1], "server") == 0) {
-        game.isServer = true;
+        game.is_server = true;
     } else {
-        game.isServer = false;
+        game.is_server = false;
     }
     if (enet_initialize() != 0) {
         TraceLog(LOG_FATAL, "An error occurred while initializing ENet");
@@ -282,7 +317,7 @@ int main(int argc, char *argv[]) {
 
         game.Update(deltaTime);
 
-        if (!game.isServer) {
+        if (!game.is_server) {
             BeginDrawing();
             game.Render();
             game.RenderImGui();
